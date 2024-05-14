bbc991216e34882b324c041124fc92e7cad4580a
Harry <hma481@aucklanduni.ac.nz>
2024-05-15 04:30:41 +1200

    fixed some code temporarily so program compiles

--- src/main/java/nz/ac/auckland/se281/HardArtificialIntelligence.java
+++ src/main/java/nz/ac/auckland/se281/HardArtificialIntelligence.java
@@ -1,5 +1,8 @@
 package nz.ac.auckland.se281;
 
 public class HardArtificialIntelligence extends ArtificialIntelligence {
-
+  @Override
+  public void play() {
+    
+  }
 }
--- src/main/java/nz/ac/auckland/se281/MediumArtificialIntelligence.java
+++ src/main/java/nz/ac/auckland/se281/MediumArtificialIntelligence.java
@@ -1,5 +1,8 @@
 package nz.ac.auckland.se281;
 
 public class MediumArtificialIntelligence extends ArtificialIntelligence {
-
+  @Override
+  public void play(){
+    
+  }
 }
