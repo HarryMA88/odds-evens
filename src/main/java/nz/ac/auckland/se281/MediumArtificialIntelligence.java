137a70b70faf6b62cb8a87e0aef9c5dcdf719f54
Harry <hma481@aucklanduni.ac.nz>
2024-05-15 19:43:13 +1200

    added the new top strategy and made mediumAI switch to top strategy on the 4th round

--- src/main/java/nz/ac/auckland/se281/MediumArtificialIntelligence.java
+++ src/main/java/nz/ac/auckland/se281/MediumArtificialIntelligence.java
@@ -1,8 +1,14 @@
 package nz.ac.auckland.se281;
 
 public class MediumArtificialIntelligence extends ArtificialIntelligence {
+  private int round = 0;
+
   @Override
   public void play() {
+    round++;
+    if (round == 4) {
+      setStrategy(new TopStrategy());
+    }
     strategy.execute();
     fingers = strategy.getFingers();
   }
---
+++ src/main/java/nz/ac/auckland/se281/TopStrategy.java
@@ -0,0 +1,15 @@
+package nz.ac.auckland.se281;
+
+public class TopStrategy implements Strategy {
+  private int fingers;
+
+  @Override
+  public void execute() {
+
+  }
+
+  @Override
+  public int getFingers() {
+    return this.fingers;
+  }
+}
